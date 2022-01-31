90b9d8ec03cf1e67adb29e2f45def48d54bca633
Jane Parsons <jane.parsons@waitrose.co.uk>
2022-01-31 15:53:42 +0000

    created test and method to instantiate a oystercard with a zero balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,12 @@
+require_relative '../lib/oystercard'
+
 describe Oystercard do
-    
+  describe '#balance' do
+    it "a new instance of oystercard has a balance of zero" do
+       oystercard = Oystercard.new 
+       expect(oystercard.balance).to eq 0  
+    end
+  end       
 end
 
 # NameError: uninitialized constant Oystercard
