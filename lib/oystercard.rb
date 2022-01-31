bf386613630f593c10c4a8ba36e6ef820ab2a4dd
Jane Parsons <jane.parsons@waitrose.co.uk>
2022-01-31 17:39:15 +0000

    Implemented touch_in and touch_out methods to change the in_journey boolean instance variable

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,9 @@
 class Oystercard
 
-
   MAXIMUM_BALANCE = 90  
-  attr_reader :balance   
+  attr_reader :balance 
+  attr_accessor :in_journey
+
 
   def initialize
     @balance = 0
@@ -11,5 +12,17 @@ class Oystercard
   def top_up(amount)
     raise "Top up failed: maximum balnce is £90" if balance + amount > MAXIMUM_BALANCE
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,8 +4,8 @@ describe Oystercard do
   
   describe '#balance' do
     it "a new instance of oystercard has a balance of zero" do
-       oystercard = Oystercard.new 
-       expect(oystercard.balance).to eq 0  
+      oystercard = Oystercard.new 
+      expect(oystercard.balance).to eq 0  
     end
   end   
   
@@ -18,6 +18,30 @@ describe Oystercard do
     it "raise an error if the top up would increase the balance beyond the maximum limit" do
       expect { subject.top_up(100) }.to raise_error "Top up failed: maximum balnce is £90"
     end
+  end
+
+  describe '#deduct' do
+    it "reduces the balance on the card by the values given" do
+      subject.top_up(50)
+      subject.deduct(25)
+      expect(subject.balance).to eq 25 
+    end    
+  end
+
+  describe '#touch_in' do
+    it "change the 'status' of the card to be in_journey?" do
+      subject.top_up(50)
+      subject.touch_in
+      expect(subject.in_journey).to eq true 
+    end    
+  end
+
+  describe '#touch_out' do
+    it "change the 'status' of the card to be in_journey?" do
+      subject.top_up(50)
+      subject.touch_out
+      expect(subject.in_journey).to eq false 
+    end    
   end
   
 end
