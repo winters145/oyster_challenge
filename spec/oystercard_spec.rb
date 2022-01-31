67f6d9c52c279fd7e2f84d7f8d1472fa453547d6
Jack <winterschladen.j@gmail.com>
2022-01-31 16:16:04 +0000

    Added a top up method to increase the balance on the card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,21 @@
 require_relative '../lib/oystercard'
 
 describe Oystercard do
+  
   describe '#balance' do
     it "a new instance of oystercard has a balance of zero" do
        oystercard = Oystercard.new 
        expect(oystercard.balance).to eq 0  
     end
-  end       
+  end   
+  
+  describe '#top_up' do
+    it "increases the balance on the card by the value given" do 
+      subject.top_up(100)
+      expect(subject.balance).to eq 100
+    end
+  end
+  
 end
 
 # NameError: uninitialized constant Oystercard
